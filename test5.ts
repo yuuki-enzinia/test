a88d75eb5f934c9cd9ac8a55dc4afcb6147860b8
yuuki <123858447+yuuki-enzinia@users.noreply.github.com>
2025-11-07 09:04:33 +0900

    Update test5.ts

--- test5.ts
+++ test5.ts
@@ -1,15 +1,1 @@
 
-export {}; // ←これだけでモジュール扱いになる
-type AnyData = {
-  [key: string]: string 
-};
-
-const a: AnyData = { name: "Alice",  };
-const b: AnyData = { name: "Alice",  };
-const c: AnyData = { name: "Alice" };
-type IsSameType<T, U> = T extends U ? (U extends T ? true : false) : false;
-
-// a と b の型をチェック
-type CheckABC = CheckAB extends true ? IsSameType<typeof a, typeof c> : false;
-type CheckAB = IsSameType<typeof a, typeof b>;
-type Result = CheckABC;
